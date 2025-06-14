0b8ee3e06c96ef1da201224d04d28580161c614f
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-06-14 12:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-// Your script here.
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('#voices');
+const options = document.querySelectorAll('[name="rate"], [name="pitch"]');
+const textarea = document.querySelector('#text');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) speechSynthesis.speak(msg);
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', () => {
+  msg.text = textarea.value;
+  toggle();
+});
+stopButton.addEventListener('click', () => toggle(false));
+
+msg.text = textarea.value;
